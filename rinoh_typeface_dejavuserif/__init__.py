74689075c47b385248746a257a6fa7911e1d463e
Brecht Machiels <brecht@mos6581.org>
2020-11-13 10:58:19 +0100

    Let rinohtype determine the font style
    The condensed DejaVu fonts are actually semi-condensed.

--- rinoh_typeface_dejavuserif/__init__.py
+++ rinoh_typeface_dejavuserif/__init__.py
@@ -1,7 +1,6 @@
 from os import path
 
 from rinoh.font import Typeface
-from rinoh.font.style import REGULAR, BOLD, ITALIC, CONDENSED
 from rinoh.font.opentype import OpenTypeFont
 
 
@@ -10,19 +9,15 @@ __all__ = ['typeface']
 
 def otf(style):
     filename = 'DejaVuSerif{}.ttf'.format(style)
-    return path.join(path.dirname(__file__), filename)
+    return OpenTypeFont(path.join(path.dirname(__file__), filename))
 
 
 typeface = Typeface('DejaVu Serif',
-                    OpenTypeFont(otf(''), weight=REGULAR),
-                    OpenTypeFont(otf('-Italic'), weight=REGULAR, slant=ITALIC),
-                    OpenTypeFont(otf('-Bold'), weight=BOLD),
-                    OpenTypeFont(otf('-BoldItalic'), weight=BOLD, slant=ITALIC),
-                    OpenTypeFont(otf('Condensed'), weight=REGULAR,
-                                 width=CONDENSED),
-                    OpenTypeFont(otf('Condensed-Italic'), weight=REGULAR,
-                                 slant=ITALIC, width=CONDENSED),
-                    OpenTypeFont(otf('Condensed-Bold'), weight=BOLD,
-                                 width=CONDENSED),
-                    OpenTypeFont(otf('Condensed-BoldItalic'), weight=BOLD,
-                                 slant=ITALIC, width=CONDENSED))
+                    otf(''),
+                    otf('-Italic'),
+                    otf('-Bold'),
+                    otf('-BoldItalic'),
+                    otf('Condensed'),
+                    otf('Condensed-Italic'),
+                    otf('Condensed-Bold'),
+                    otf('Condensed-BoldItalic'))
